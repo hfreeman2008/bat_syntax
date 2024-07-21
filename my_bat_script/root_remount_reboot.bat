a99eaafb10860dd4c9da846f9b2f19ba5b2e9161
hfreeman2008 <hfreeman2008@126.com>
2024-07-21 19:15:35 +0800

    bat

---
+++ my_bat_script/root_remount_reboot.bat
@@ -0,0 +1,16 @@
+:: author hexiaoming
+:: push app to phone
+@title "hexiaoming root remount"
+@color 0f
+
+::@echo off
+
+adb devices
+adb wait-for-device
+adb root
+adb wait-for-device
+adb remount
+adb wait-for-device
+adb reboot
+
+pause
